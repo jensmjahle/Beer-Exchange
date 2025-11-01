835fe4b6d85d0026bfe4db45ca2f222cadcdd805
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-11-01 19:35:25 +0100

    fix buy modal

--- src/components/modals/BuyBeerModal.vue
+++ src/components/modals/BuyBeerModal.vue
@@ -16,6 +16,30 @@
         </select>
       </div>
 
+      <div>
+        <label for="customer">Velg kunde:</label>
+        <select id="customer" v-model="selectedCustomer" class="w-full mb-2 border rounded px-2 py-1">
+          <option disabled value="">-- velg kunde --</option>
+          <option v-for="c in customers" :key="c.id" :value="c.id">
+            {{ c.name }}
+          </option>
+        </select>
+      </div>
+
+
+      <div v-if="beer?.volumes?.length">
+        <label for="volume">Velg volum:</label>
+        <select id="volume" v-model="selectedVolume">
+          <option
+            v-for="v in costumers"
+            :key="v.volume_ml"
+            :value="v.volume_ml"
+          >
+            {{ v.name}} ml
+          </option>
+        </select>
+      </div>
+
       <label for="qty">Antall</label>
       <input id="qty" type="number" v-model.number="qty" min="1" />
 
@@ -33,19 +57,40 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import {ref, computed, onMounted, watch} from 'vue'
 import { createTransaction } from '@/services/transactions.service'
+import {listCustomers} from "@/services/customers.service.js";
 
 const props = defineProps({
   visible: Boolean,
   beer: Object,
-  customerId: String,
   eventId: String,
 })
 const emit = defineEmits(['close', 'bought'])
 
 const qty = ref(1)
 const selectedVolume = ref(props.beer?.volumes?.[0]?.volume_ml || 500)
+const customers = ref([])
+const loadingCustomers = ref(false)
+const selectedCustomer = ref('')
+
+async function loadCustomers() {
+  try {
+    loadingCustomers.value = true
+    const rows = await listCustomers(props.eventId)
+    customers.value = Array.isArray(rows) ? rows : []
+    selectedCustomer.value = ''
+  } catch (e) {
+    console.error('Feil ved henting av kunder:', e)
+    alert('Kunne ikke hente kundeliste')
+  } finally {
+    loadingCustomers.value = false
+  }
+}
+
+watch(() => props.visible, (v) => { if (v) loadCustomers() })
+onMounted(() => { if (props.visible) loadCustomers() })
+
 
 const displayPrice = computed(() => {
   const liters = selectedVolume.value / 1000
@@ -53,11 +98,16 @@ const displayPrice = computed(() => {
 })
 
 async function buy() {
+  if (!selectedCustomer.value) {
+    alert('Velg en kunde først.')
+    return
+  }
+
   try {
     const res = await createTransaction({
       event_id: props.eventId,
       event_beer_id: props.beer.id,
-      customer_id: props.customerId,
+      customer_id: selectedCustomer.value,
       qty: qty.value,
       volume_ml: selectedVolume.value,
     })
